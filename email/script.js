7c024a7182d252eefed402540c0c9bf8e0dfbcfa
arunpandeyg <128284476+arunpandeyg@users.noreply.github.com>
2024-05-03 17:18:49 +0530

    Update script.js

--- email/script.js
+++ email/script.js
@@ -1,6 +1,6 @@
 function sendEmail() {
     Email.send({
-        SecureToken:"4ba05a9c-ff4b-4f2d-a821-73140cdd77d8",
+        SecureToken:"put here your elestic email token",
     //   Host: "smtp.gmail.com",
     //   Username: "arunpandeyntl@gmail.com",
     //   Password: "D85B8B1956869C10832D79DB7F93DB5F1EB7",
