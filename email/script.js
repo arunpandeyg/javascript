509affc31293d404c333792d26f630c54f09af5f
Arun Pandey <128284476+arunpandeyg@users.noreply.github.com>
2025-10-07 07:45:43 +0530

    Update script.js

--- email/script.js
+++ email/script.js
@@ -2,8 +2,8 @@ function sendEmail() {
     Email.send({
         SecureToken:"put here your elestic email token",
     //   Host: "smtp.gmail.com",
-    //   Username: "arunpandeyntl@gmail.com",
-    //   Password: "D85B8B1956869C10832D79DB7F93DB5F1EB7",
+    //   Username: "",
+    //   Password: "",
       To: "arunpandeyom@gmail.com.com",
       From: document.getElementById("email").value,
       Subject: "New contact form enquiry",
